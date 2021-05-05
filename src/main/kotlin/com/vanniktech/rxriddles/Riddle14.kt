77dd0c5bff5e538e24525685e4d87defd42b8613
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-05-05 13:39:31 +0300

    Solve riddle 14

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle14.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle14.kt
@@ -1,14 +1,17 @@
 package com.vanniktech.rxriddles
 
 import io.reactivex.rxjava3.core.Single
+import java.lang.IllegalArgumentException
 
 object Riddle14 {
-  /**
-   * Try the given [source] up to three times unless an [IllegalArgumentException] has been emitted.
-   *
-   * Use case: Retry an operation for a number of times or until a valid error occurred.
-   */
-  fun solve(source: Single<Unit>): Single<Unit> {
-    TODO()
-  }
+    /**
+     * Try the given [source] up to three times unless an [IllegalArgumentException] has been emitted.
+     *
+     * Use case: Retry an operation for a number of times or until a valid error occurred.
+     */
+    fun solve(source: Single<Unit>): Single<Unit> {
+        return source.retry(2) {
+            it !is IllegalArgumentException
+        }
+    }
 }
