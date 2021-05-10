2e1292fb909ea6d47a1c20a6890de461430eec9b
Orășan Corina <62331686+corinaorasan@users.noreply.github.com>
2021-05-10 14:50:02 +0300

    Solve riddle 29 (#24)

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle29.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle29.kt
@@ -3,12 +3,10 @@ package com.vanniktech.rxriddles
 import io.reactivex.rxjava3.core.Maybe
 
 object Riddle29 {
-  /**
-   * Call the given [function] when the [source] errors.
-   *
-   * Use case: Add some logging.
-   */
-  fun solve(source: Maybe<Int>, function: (Throwable) -> Unit): Maybe<Int> {
-    TODO()
-  }
+    /**
+     * Call the given [function] when the [source] errors.
+     *
+     * Use case: Add some logging.
+     */
+    fun solve(source: Maybe<Int>, function: (Throwable) -> Unit): Maybe<Int> = source.doOnError(function)
 }
