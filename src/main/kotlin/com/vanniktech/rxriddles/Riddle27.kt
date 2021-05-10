c87e8be8f76bcc00caa9c0cdf7c221f2de64bc22
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-05-10 14:33:23 +0300

    Solve riddle 27

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle27.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle27.kt
@@ -1,14 +1,16 @@
 package com.vanniktech.rxriddles
 
 import io.reactivex.rxjava3.core.Observable
+import jdk.nashorn.internal.objects.NativeDebug.map
 
 object Riddle27 {
-  /**
-   * Call the given [function] each time the [source] emits a value.
-   *
-   * Use case: Add some logging.
-   */
-  fun solve(source: Observable<Long>, function: (Long) -> Unit): Observable<Long> {
-    TODO()
-  }
+    /**
+     * Call the given [function] each time the [source] emits a value.
+     *
+     * Use case: Add some logging.
+     */
+    fun solve(source: Observable<Long>, function: (Long) -> Unit): Observable<Long> {
+        return source.doOnNext(function)
+    }
 }
+
