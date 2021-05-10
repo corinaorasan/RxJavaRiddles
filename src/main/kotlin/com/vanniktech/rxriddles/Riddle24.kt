90e0fd096751c24f067cc9f7fbfbab395bdb6e03
Orășan Corina <62331686+corinaorasan@users.noreply.github.com>
2021-05-10 14:03:55 +0300

    Solve riddle 24 (#19)

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle24.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle24.kt
@@ -4,12 +4,12 @@ import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 
 object Riddle24 {
-  /**
-   * Upon completion of the [source] return a Single containing the number of emissions from [source].
-   *
-   * Use case: Know how many emissions have been sent out.
-   */
-  fun solve(source: Observable<Any>): Single<Long> {
-    TODO()
-  }
+    /**
+     * Upon completion of the [source] return a Single containing the number of emissions from [source].
+     *
+     * Use case: Know how many emissions have been sent out.
+     */
+    fun solve(source: Observable<Any>): Single<Long> {
+        return source.count()
+    }
 }
