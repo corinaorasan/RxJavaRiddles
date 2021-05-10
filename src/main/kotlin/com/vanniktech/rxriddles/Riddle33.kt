bae9e068fc69ea2f237931bcaa64601d54f80d25
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-05-10 15:29:11 +0300

    Solve riddle 33

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle33.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle33.kt
@@ -4,12 +4,12 @@ import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Scheduler
 
 object Riddle33 {
-  /**
-   * Shift all downstream work of [source] to the given [scheduler].
-   *
-   * Use case: You want to shift work to a particular [Scheduler].
-   */
-  fun solve(source: Completable, scheduler: Scheduler): Completable {
-    TODO()
-  }
+    /**
+     * Shift all downstream work of [source] to the given [scheduler].
+     *
+     * Use case: You want to shift work to a particular [Scheduler].
+     */
+    fun solve(source: Completable, scheduler: Scheduler): Completable {
+        return source.observeOn(scheduler)
+    }
 }
